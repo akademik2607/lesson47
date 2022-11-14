49bcbe31fa2937dfd4381663636cd495918eefea
Alexey Illarionov <akademik2607@mail.ru>
2022-11-15 00:45:48 +0300

    Add test data, create ads models

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from ads.models import Category, Location, User, Advertisement
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(User)
+admin.site.register(Advertisement)
